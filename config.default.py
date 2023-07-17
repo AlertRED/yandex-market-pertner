d291bb22ea3277076c6e44ad88e6b6fc9b2f07f7
Aleksandr <drammtv@gmail.com>
2023-07-17 19:24:16 +0300

    Added special variables to config

--- config.default.py
+++ config.default.py
@@ -1,1 +1,6 @@
-MARKET_SELLER_INN: str = None  # sellerInn
+MARKET_SELLER_INN: str = None
+MARKET_CAMPAIGN_ID: int = None
+MARKET_ACCESS_TOKEN: str = None
+
+GOOGLE_SPREADSHEET_ID: str = None
+GOOGLE_TOKEN_FILE_NAME: str = None
